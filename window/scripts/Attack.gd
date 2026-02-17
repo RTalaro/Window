aff837ace97b8dd75f3e4ca3254e1d89c40a12b2
al wen <al.wenn05@gmail.com>
2026-02-17 00:05:47 -0800

    added health and damage to enemies, minor bug fixes

--- window/scripts/Attack.gd
+++ window/scripts/Attack.gd
@@ -1,6 +1,6 @@
 class_name Attack
 
 var attack_damage : float = 10.0
-var Knockback_force : float = 100.0
+var knockback_force : float = 100.0
 var attack_position : Vector2 = Vector2(0,0)
 var stun_time : float = 1.0
--- window/scripts/player/weapons/bullet.gd
+++ window/scripts/player/weapons/bullet.gd
@@ -18,19 +18,6 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 
-func _on_hitbox_area_entered(area):
-	if area is HitboxComponent:
-		var hitbox : HitboxComponent = area
-		
-		var attack = Attack.new()
-		attack.attack_damage = attack_damage
-		attack.knockback_force = knockback_force
-		attack.attack_position = global_position
-		attack.stun_time = stun_time
-		
-		hitbox.damage(attack)
-
-
 func despawn() -> void:
 	await get_tree().create_timer(despawn_time).timeout
 	queue_free()
@@ -41,3 +28,16 @@ func _on_target_collision_body_entered(body: Node2D) -> void:
 		queue_free()
 	piercing -= 1
 		
+
+
+func _on_target_collision_area_entered(area):
+	if area is HitboxComponent:
+		var hitbox : HitboxComponent = area
+		
+		var attack = Attack.new()
+		attack.attack_damage = attack_damage
+		attack.knockback_force = knockback_force
+		attack.attack_position = global_position
+		attack.stun_time = stun_time
+		
+		hitbox.damage(attack)
